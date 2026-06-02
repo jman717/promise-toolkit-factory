732de5504cdc5a543dfe681b0db36961621af34a
jman717 <jim.manton@skybeam.com>
2026-06-01 20:25:08 -0600

    new tests, new factory in the toolkit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+[![npm Package](https://img.shields.io/npm/v/log4js-tagline.svg)](https://www.npmjs.org/package/promise-toolkit-factory)
+[![License](https://img.shields.io/npm/l/promise-toolkit-factory.svg)](https://github.com/jman717/promise-toolkit-factory/blob/master/LICENSE)
+[![CodeQL](https://github.com/jman717/promise-toolkit-factory/actions/workflows/actions.yml/badge.svg)](https://github.com/jman717/promise-toolkit-factory/actions/workflows/actions.yml)
+[![Node.js CI](https://github.com/jman717/promise-toolkit-factory/actions/workflows/node.js.yml/badge.svg)](https://github.com/jman717/promise-toolkit-factory/actions/workflows/node.js.yml)
+
+[![NPM](https://nodei.co/npm/promise-toolkit-factory.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/promise-toolkit-factory/)
 
 Under a promise structure, map functions to create flows free from callbacks; the factory class objects are then lightweight decoupled objects for any new micro-service arrangement.
 
@@ -13,144 +19,11 @@ Installation
 ---------
 ```
 npm install promise-toolkit-factory
-
 ```
 
-Functions in the factory follow this format:
-
-```javascript
-class exampleC{
-	func1(json_object){
-			//code goes here
-			json_object.resolve(json_object);
-		});
-	}
-
-	func2(json_object){
-		//code goes here
-		json_object.resolve(json_object);
-	}
-
-	func3(json_object){
-		//code goes here
-		json_object.reject('some error message');
-	}
-}
-```
-
-The following example would have this setup:
-
-```javascript
-var exC = new exampleC();
-
-....
-	.appender('classes', {"objs": [{"name": "c1", "obj": exC}])
-	.appender('functions', {"flow": 1, "map": ["c1.func1", "c1.func2", "c1.func3"]})
-....
-```
-
-Values can be passed from one function, or to all functions in any particular flow, as:
-
-```javascript
-class exampleC{
-	func1(json_object){
-		json_object.new_value = 4;
-		json_object.resolve(json_object);
-	}
-
-	func2(json_object){
-		if(json_object.new_value > 3)
-			throw new Error('got an error');
-		json_object.resolve(json_object);
-	}
-}
-```
-
-The error will throw back to the latest promise which will catch and return a reject for that promise.
-
-## Usage Sample 1
-
-This defines 2 class objects and 4 function flows. Each flow is done in sequence, with a json object passed to each subsequence function. A fork can simply be values passed to the appropriate flow.
-
-```js
-var log4js = require("log4js")
-		,log4js_tagline = require("log4js-tagline");
-		,ptf = require('promise-toolkit-factory');
-
-		mp = t.getMainParams(req, res);
-		var f1 = require(t.factory_dir + '/promise_test1');
-		var f2 = require(t.factory_dir + '/promise_test2');
-		var f1o = new f1();
-		var f2o = new f2();
-		var toolkit = new ptf().appender('logging', {"type": "log4js-tagline", "log": t.log, "logger": t.logger})
-			.appender('vars', {"globals": mp, "local": {"total": 0}})
-			.appender('classes', {"objs": [{"name": "f1o", "obj": f1o}, {"name": "f2o", "obj": f2o}]})
-			.appender('functions', {"flow": 1, "map": ["f1o.init", "f2o.process", "f1o.step1", "f1o.step4", "f2o.set_init_opts"]})
-			.appender('functions', {"flow": 2, "map": ["f1o.init", "f1o.step2", "f1o.step5"]})
-			.appender('functions', {"flow": 3, "map": ["f1o.init", "f1o.finish"]})
-			.appender('functions', {"flow": 4, "map": ["f1o.init", "f1o.do_this"]})
-			.appender('promises', {"flows":[1, 2, 3]})
-			.appender('promises', {"flows":[4]})
-			.run();
-
-		var toolkit = new ptf().appender('logging', {"type": "log4js-tagline", "log": t.log, "logger": t.logger})
-			.appender('vars', {"globals": mp, "local": {"total": 0}})
-			.appender('class', {"obj": {"name": "f1o", "obj": f1o}})
-			.appender('cron', {"name": "cron", "schedule": "* * * * * *")
-			.appender('function', {"name", "flo.init", "params": {}}).on("success", function({ data }){
-				}).on("error", function({ err }){
-					err.describe()
-				})
+General Setup Tests. 
+---------
 ```		
-
-With multiple flows, skip_to_flow bypasses any further processing to go directly to the indicated flow. 
-
-```js
-class exampleC{
-	func1(json_object){
-		var g = json_object.parent.getVars({"vars":"globals"})
-			,loc = json_object.parent.getVars({"vars":"local"});
-		loc.total = 4;
-		json_object.resolve(json_object);
-	}
-
-	func2(json_object){
-		var g = json_object.parent.getVars({"vars":"globals"})
-			,loc = json_object.parent.getVars({"vars":"local"});
-		if(loc.total > 3)
-			json_object.skip_to_flow = 2;
-		json_object.resolve(json_object);
-	}
-}
-```		
-
-The error will throw back to the latest promise which will catch and return a reject for that promise.
-		
-
-## Usage Sample 2
-
-This defines 2 class objects and 4 function flows from above but no file logging is defined. 
-
-```js
-
-const ptf = require("promise-toolkit-factory");
-
-var f1 = require('./lib/factory/promise_test1')
-		,f2 = require('./lib/factory/promise_test2');
-
-var f1o = new f1()
-		,f2o = new f2();
-
-var toolkit = new ptf().appender('vars', {"globals": {"any":"values"}})
-	.appender('classes', {"objs": [{"name": "f1o", "obj": f1o}, {"name": "f2o", "obj": f2o}]})
-	.appender('functions', {"flow": 1, "map": ["f1o.init", "f2o.process", "f1o.step1", "f1o.step4", "f2o.set_init_opts"]})
-	.appender('functions', {"flow": 2, "map": ["f1o.init", "f1o.step2", "f1o.step5"]})
-	.appender('functions', {"flow": 3, "map": ["f1o.init", "f1o.finish"]})
-	.appender('functions', {"flow": 4, "map": ["f1o.init", "f1o.do_this"]})
-	.appender('promises', {"flows":[1, 2, 3]})
-	.appender('promises', {"flows":[4]})
-	.run();
-
-console.log("test complete");
-
+npm run test_a
+npm run test_b
 ```		
--- app.js
+++ app.js
@@ -5,113 +5,113 @@
 * @since 2018-01-08
 */
 
-module.exports = class promise_toolkit_factory{
+module.exports = class promise_toolkit_factory {
 	constructor(jo) {
 		var t = this;
-    t.appenders_dir = './lib/appenders/';
+		t.appenders_dir = './lib/appenders/';
 		t.appenders = [];
 		t._log = null;
 		return t;
 	}
 
-	appender(name, conf){
+	appender(name, conf) {
 		var t = this;
-		try{
-			if(typeof eval('t.' + name) === 'undefined'){
+		try {
+			if (typeof eval('t.' + name) === 'undefined') {
 				var a = t.appenders_dir + name + '.js';
 				var x = require(a);
 				t.appenders.push(new x(t).config(conf).init());
-				t.log({"type":"trace", "text": "file loading=" + a, "classO":"promise_toolkit_factory.appender", "file":"app.js"});
+				t.log({ "type": "trace", "text": "file loading=" + a, "classO": "promise_toolkit_factory.appender", "file": "app.js" });
 			}
 			return t;
-		}catch(e){
+		} catch (e) {
 			console.log('appenders error: ' + e.message);
 		}
 	}
-	
-	log(jo){
-		try{
+
+	log(jo) {
+		try {
 			var t = this;
-			if(t._log !== null)
+			if (t._log !== null) {
 				t._log.msg(jo);
-			else{
-				if(typeof jo !== 'undefined' && typeof jo.text !== 'undefined')
+			} else {
+				if (typeof jo !== 'undefined' && typeof jo.text !== 'undefined')
 					console.log('logger is undefined. Message: ' + jo.text);
-			}			
-		}catch(e){
-			console.log('log error: ' + e.message);
+			}
+		} catch (e) {
+			console.log('app.js log error: ' + e.message);
 		}
 	}
-	
-	run(){
+
+	run() {
 		var t = this;
 		var a = t.appenders, z;
-		try{
-			t.log({"type":"trace", "text": "run()", "classO":"promise_toolkit_factory.run", "file":"app.js"});			
-			for(z = 0; z < a.length; z++)
-				if(a[z].oname === 'promises')						
+		try {
+			t.log({ "type": "trace", "text": `run() count(${a.length})`, "classO": "promise_toolkit_factory.run", "file": "app.js" });
+			for (z = 0; z < a.length; z++)
+				if (a[z].oname === 'promises')
 					a[z].run();
 			return t;
-		}catch(e){
-			t.log({"type":"error", "text": "error: " + e.message, "classO":"promise_toolkit_factory.run", "file":"app.js"});
+		} catch (e) {
+			t.log({ "type": "error", "text": "error: " + e.message, "classO": "promise_toolkit_factory.run", "file": "app.js" });
 		}
 	}
-	
-	getFlowAppender(jo){
+
+	getFlowAppender(jo) {
 		var t = this;
 		var a = t.appenders, z;
-		try{
-			if(typeof jo.flow === 'undefined')
+		try {
+			if (typeof jo.flow === 'undefined')
 				throw new Error('jo.flow is undefined');
-			t.log({"type":"trace", "text": "run()", "classO":"promise_toolkit_factory.getFlowAppender", "file":"app.js"});			
-			for(z = 0; z < a.length; z++)
-				if(a[z].oname === 'functions')						
-					if(a[z].conf.flow === jo.flow)
+			t.log({ "type": "trace", "text": "run()", "classO": "promise_toolkit_factory.getFlowAppender", "file": "app.js" });
+			for (z = 0; z < a.length; z++)
+				if (a[z].oname === 'functions')
+					if (a[z].conf.flow === jo.flow)
 						return a[z];
 			throw new Error("cannot find flow(" + jo.flow + ").");
-		}catch(e){
-			t.log({"type":"error", "text": "error: " + e.message, "classO":"promise_toolkit_factory.getFlow", "file":"app.js"});
+		} catch (e) {
+			t.log({ "type": "error", "text": "error: " + e.message, "classO": "promise_toolkit_factory.getFlow", "file": "app.js" });
 		}
 	}
-	
-	getVars(jo){
+
+	getVars(jo) {
 		var t = this;
 		var a = t.appenders, z;
-		try{
-			if(typeof jo === 'undefined')
+		try {
+			if (typeof jo === 'undefined')
 				throw new Error('jo is undefined');
-			if(typeof jo.vars === 'undefined')
+			if (typeof jo.vars === 'undefined')
 				throw new Error('jo.vars is undefined');
-			t.log({"type":"trace", "text": "run()", "classO":"promise_toolkit_factory.getVars", "file":"app.js"});			
-			for(z = 0; z < a.length; z++)
-				if(a[z].oname === 'vars')						
-					if(typeof a[z].conf[jo.vars] !== 'undefined')
+			t.log({ "type": "trace", "text": "run()", "classO": "promise_toolkit_factory.getVars", "file": "app.js" });
+			for (z = 0; z < a.length; z++)
+				if (a[z].oname === 'vars')
+					if (typeof a[z].conf[jo.vars] !== 'undefined')
 						return a[z].conf[jo.vars];
 			throw new Error("cannot find vars(" + jo.vars + ").");
-		}catch(e){
-			t.log({"type":"error", "text": "error: " + e.message, "classO":"promise_toolkit_factory.getFlow", "file":"app.js"});
+		} catch (e) {
+			t.log({ "type": "error", "text": "error: " + e.message, "classO": "promise_toolkit_factory.getFlow", "file": "app.js" });
 		}
 	}
-	
-	find_class(s){
+
+	find_class(s) {
 		var t = this;
 		var z, y, a = t.appenders, ob;
-		try{
-			t.log({"type":"trace", "text": "looking for class(" + s + ')', "classO":"promise_toolkit_factory.find_class", "file":"app.js"});			
-			for(z = 0; z < a.length; z++){
-				if(a[z].oname === 'classes'){						
+		try {
+			t.log({ "type": "trace", "text": "looking for class(" + s + ')', "classO": "promise_toolkit_factory.find_class", "file": "app.js" });
+			for (z = 0; z < a.length; z++) {
+				if (a[z].oname === 'classes') {
 					ob = a[z].conf.objs;
-					for(y = 0; y < ob.length; y++){
-						if(s === ob[y].name){
-							t.log({"type":"debug", "text": "function(" + s + ")=(" + ob[y].name + ") confirmed", "classO":"functions.init", "file":"functions.js"});	
+					for (y = 0; y < ob.length; y++) {
+						if (s === ob[y].name) {
+							t.log({ "type": "debug", "text": "function(" + s + ")=(" + ob[y].name + ") confirmed", "classO": "functions.init", "file": "functions.js" });
 							return ob[y].obj;
 						}
 					}
 				}
 			}
 			return null;
-		}catch(e){
-			t.log({"type":"error", "text": "error: " + e.message, "classO":"promise_toolkit_factory.find_class", "file":"app.js"});
+		} catch (e) {
+			t.log({ "type": "error", "text": "error: " + e.message, "classO": "promise_toolkit_factory.find_class", "file": "app.js" });
 		}
 	}
 }
